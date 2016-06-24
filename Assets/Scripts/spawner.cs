1ce92a8cee181ca2c72462834eddd477f9d7dc50
Tanguy Gauvrit <tanguygauvrit@gmail.com>
2016-06-24 23:41:06 +0200

    Enemies

---
+++ Assets/Scripts/spawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawner : MonoBehaviour {
+
+	private diabloUnit liveUnit;
+	public diabloUnit unit;
+
+	private bool spawning = false;
+	public float spawnDelay = 3f;
+
+	void OnDrawGizmos() {
+		Gizmos.DrawIcon (transform.position, "death.png");
+	}
+
+	// Use this for initialization
+	void Start () {
+		spawn ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (liveUnit == null && !spawning) {
+			spawning = true;
+			Invoke("spawn", spawnDelay);
+		}
+	}
+
+	private void spawn() {
+		liveUnit = GameObject.Instantiate (unit, transform.position, transform.rotation) as diabloUnit;
+		spawning = false;
+	}
+}
