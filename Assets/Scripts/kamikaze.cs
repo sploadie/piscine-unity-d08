cce2ffc7302ae546ebdf36b4ba67f839af510e90
Tanguy Gauvrit <tanguygauvrit@gmail.com>
2016-06-25 20:34:20 +0200

    Added boom

---
+++ Assets/Scripts/kamikaze.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class kamikaze : MonoBehaviour {
+
+	public float deleteAfter;
+
+	// Use this for initialization
+	void Start () {
+		GameObject.Destroy (this.gameObject, deleteAfter);
+	}
+}
